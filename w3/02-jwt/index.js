0d63c6ec04e14898a5cfdbd25d93f6bbd2aa4dad
Teja Reddy Kolli <tejak73198@gmail.com>
2023-12-30 13:31:59 -0600

    a1_jwt-mregarding the working of the json web token

---
+++ w3/02-jwt/index.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const jwtPassword = "secret";
+const zod= require("zod");
+
+const emailSchema =zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function signJwt(username, password) {
+const usernameResponse = emailSchema.safeParse(username);
+const passwordResponse = passwordSchema.safeParse(password);
+if (!usernameResponse.success || !passwordResponse.success) 
+{ return null;}
+
+const signature = jwt.signJWt({ username}, jwtPassword)
+return signature;
+}
+
+
+function verifyJwt(token) {
+    try { jwt.verify(token, jwtPassword);
+    return true; }
+ 
+catch(e){}
+    {return false;}
+    }
+
+function decodeJwt(token) {
+    const decoded =jwt.decode(token);
+    if(decoded)
+    { return true;}
+    else {return false;}
+}
+module.exports = {
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
